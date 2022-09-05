38696bf58d4d3d22dfb8b26e9c527cffb093055b
WhatIsLove17 <WhatIsLoveff@gmail.com>
2022-09-06 00:28:07 +0300

    add auto configuration default authentication

--- src/main/java/org/whatIsLove/config/SecurityConfig.java
+++ src/main/java/org/whatIsLove/config/SecurityConfig.java
@@ -1,22 +1,35 @@
 package org.whatIsLove.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.whatIsLove.security.AuthProviderImpl;
+import org.whatIsLove.services.PersonDetailsService;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final AuthProviderImpl authProvider;
+    //private final AuthProviderImpl authProvider;
+
+    private final PersonDetailsService personDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(AuthProviderImpl authProvider, PersonDetailsService personDetailsService) {
+        //this.authProvider = authProvider;
+        this.personDetailsService = personDetailsService;
     }
 
-    protected void configure(AuthenticationManagerBuilder auth){
-        auth.authenticationProvider(authProvider);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService);
+        //auth.authenticationProvider(authProvider);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
