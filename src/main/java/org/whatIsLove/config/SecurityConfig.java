4547e72a66b5e4fcddcad78a6912966151dfc3b4
WhatIsLove17 <WhatIsLoveff@gmail.com>
2022-09-06 21:25:48 +0300

    add custom login page without scrf token

--- src/main/java/org/whatIsLove/config/SecurityConfig.java
+++ src/main/java/org/whatIsLove/config/SecurityConfig.java
@@ -3,12 +3,14 @@ package org.whatIsLove.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.whatIsLove.security.AuthProviderImpl;
 import org.whatIsLove.services.PersonDetailsService;
+
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -21,6 +23,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public SecurityConfig(AuthProviderImpl authProvider, PersonDetailsService personDetailsService) {
         //this.authProvider = authProvider;
         this.personDetailsService = personDetailsService;
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/auth/login", "/error").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/books", true)
+                .failureUrl("/auth/login?error");
     }
 
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
